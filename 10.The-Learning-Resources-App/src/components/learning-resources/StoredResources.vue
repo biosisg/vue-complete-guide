0c9cca35d8f8a35816aa7233015ad443a45a8aae
Biosis Lanjanirina <biosis.lanjanirina@icloud.com>
2023-10-08 18:31:34 +0300

    10. Course Project - The Learning Resources App

---
+++ 10.The-Learning-Resources-App/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+        <learning-resources v-for="res in resources" :key="res.id" :id="res.id" :title="res.title"
+                            :description="res.description" :link="res.link"></learning-resources>
+    </ul>
+</template>
+
+<script>
+    import LearningResources from "./LearningResources";
+
+    export default {
+        name: "StoredResources",
+        components: {LearningResources},
+        inject: ['resources']
+    }
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        margin: auto;
+        max-width: 40rem;
+    }
+</style>
